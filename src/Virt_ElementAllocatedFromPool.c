199c9e6f09db3782f18264ffc77b382305f2c877
Dan Smith <danms@us.ibm.com>
2007-11-20 06:41:27 -0800

    Fix uses of get_typed_*() in ElementAllocatedFromPool
    Signed-off-by: Dan Smith <danms@us.ibm.com>

--- src/Virt_ElementAllocatedFromPool.c
+++ src/Virt_ElementAllocatedFromPool.c
@@ -257,6 +257,7 @@ static CMPIInstance *make_ref(const CMPIObjectPath *ref,
         CMPIInstance *refinst = NULL;
 
         refinst = get_typed_instance(_BROKER,
+                                     CLASSNAME(ref),
                                      "ElementAllocatedFromPool",
                                      NAMESPACE(ref));
 
