61d441ca83ede9b1425934f0a568635e19849b69
Kaitlin Rupert <karupert@us.ibm.com>
2007-12-21 10:58:37 -0800

    Add check in RAFP to ensure ref passed is an actual instance.
    RAFP currently returns instances for invalid refs where the InstanceID doesn't match the classname type (see failing query).
    
    Get the ResourcePool instance that corresponds to the ref using get_pool_inst().  If this fails, the reference is not valid.
    
    Failing query:
    wbemcli ain -ac Xen_ResourceAllocationFromPool 'http://localhost:5988/root/virt:Xen_ProcessorPool.InstanceID="MemoryPool/0"'
    
    Signed-off-by: Kaitlin Rupert <karupert@us.ibm.com>
    
     Virt_ResourceAllocationFromPool.c |    4 ++++
     1 file changed, 4 insertions(+)

--- src/Virt_ResourceAllocationFromPool.c
+++ src/Virt_ResourceAllocationFromPool.c
@@ -183,6 +183,7 @@ static CMPIStatus pool_to_rasd(const CMPIObjectPath *ref,
         CMPIStatus s = {CMPI_RC_OK, NULL};
         const char *poolid;
         uint16_t type;
+        CMPIInstance *inst;
 
         if (!match_hypervisor_prefix(ref, info))
                 return s;
@@ -202,6 +203,9 @@ static CMPIStatus pool_to_rasd(const CMPIObjectPath *ref,
                 goto out;
         }
 
+        s = get_pool_inst(_BROKER, ref, &inst);
+        if ((s.rc != CMPI_RC_OK) || (inst == NULL))
+                goto out;
 
         rasds_from_pool(type, 
                         ref,
